ce98828a3da57241db2e6d4a05fd7afa1e17d234
Artem-Kalugin <Aharlo4@gmail.com>
2020-11-01 19:31:02 +0300

    task: do viginere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,41 @@
-const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(machine = 'true') {
+    this.machine = machine;
+  }
+  encrypt(message, key) {
+    let lenghtMessage = message.length;
+    const lenghtKey = key.length;
+    let res = '';
+    let i = 0; 
+    let j = 0;
+    while (lenghtMessage--) {
+      if (!/[a-zA-Z]/.test(message[i])) {
+        res += message[i];
+        ++j;
+      } else {
+      res += String.fromCharCode((message[i].toUpperCase().charCodeAt() + key[(i - j) % lenghtKey].toUpperCase().charCodeAt() - 130) % 26 + 65);
+      }
+      ++i;
+    }
+    return this.machine ? res : res.split('').reverse().join('');
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message, key) {
+    let lenghtMessage = message.length;
+    const lenghtKey = key.length;
+    let res = '';
+    let i = 0; 
+    let j = 0;
+    while (lenghtMessage--) {
+      if (!/[a-zA-Z]/.test(message[i])) {
+        res += message[i];
+        ++j;
+      } else {
+      res += String.fromCharCode(((message[i].toUpperCase().charCodeAt() - key[(i - j) % lenghtKey].toUpperCase().charCodeAt()) + 26) % 26 + 65);
+      }
+      ++i;
+    }
+    return this.machine ? res : res.split('').reverse().join('');
   }
 }
-
 module.exports = VigenereCipheringMachine;
